0e07b097da93a97197eaf47792acf7030ebc61f5
Joseph Mearman <joseph@mearman.co.uk>
2026-03-24 18:00:52 +0000

    feat(ranking): add baselines barrel export module
    Create new src/ranking/baselines/index.ts to aggregate baseline ranking
    implementations (shortest, degree sum, widest path, etc.). This enables
    clean re-export from the parent ranking module and establishes baselines
    as a first-class module within the ranking package.

---
+++ src/ranking/baselines/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Baseline ranking methods module.
+ *
+ * Provides simple ranking strategies for comparing discovered paths,
+ * useful for evaluating PARSE against established graph metrics.
+ *
+ * @module ranking/baselines
+ */
+
+export * from "./types";
+export * from "./shortest";
+export * from "./degree-sum";
+export * from "./widest-path";
+export * from "./jaccard-arithmetic";
+export * from "./pagerank";
+export * from "./betweenness";
+export * from "./katz";
+export * from "./communicability";
+export * from "./resistance-distance";
+export * from "./random-ranking";
